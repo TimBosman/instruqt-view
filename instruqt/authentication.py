6713b62a5673e07cfbc0fb398e4bcaf020c90ba7
Tim <tim.bosman@elastic.co>
2026-09-10 10:45:05 +0200

    Implement reauthentication for API requests and handle token refresh

--- instruqt/authentication.py
+++ instruqt/authentication.py
@@ -2,8 +2,12 @@
 
 import json
 import os
+import subprocess
 
 def read_api_key(conf_file="~/.config/instruqt/credentials"):
     with open(os.path.expanduser(conf_file), "r") as openfile:
         config = json.load(openfile)
     return config.get("access_token")
+
+def reroll_api_key():
+    subprocess.run(["instruqt", "track", "push"])
--- instruqt/instruqt.py
+++ instruqt/instruqt.py
@@ -7,10 +7,21 @@ class Instruqt:
         self.api_key = authentication.read_api_key(conf_file)
         self.variables = {"teamSlug": team}
 
+    def __reauthenticate(self):
+        authentication.reroll_api_key()
+        self.api_key = authentication.read_api_key()
+
     def request(self, body, url="https://play.instruqt.com/graphql", **kwargs):
         headers = kwargs.pop("headers", {})
         headers["Authorization"] = f"Bearer {self.api_key}"
-        return requests.post(url, headers=headers, json=body, **kwargs).json()
+        response = requests.post(url, headers=headers, json=body, **kwargs)
+        if response.status_code != 200:
+            self.__reauthenticate()
+            headers["Authorization"] = f"Bearer {self.api_key}"
+            response = requests.post(url, headers=headers, json=body, **kwargs)
+            if response.status_code != 200:
+                raise Exception(f"Request failed with status code {response.status_code}")
+        return response.json()
 
     def get_invites(self, include_expired=False, include_upcoming=True):
         body = {
