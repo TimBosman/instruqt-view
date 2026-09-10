acec335d6da805c1e29144770574e3e8db852646
Tim <tim.bosman@elastic.co>
2026-09-10 10:31:29 +0200

    Not print the full JSON response

--- __main__.py
+++ __main__.py
@@ -7,7 +7,6 @@ from tabulate import tabulate
 if __name__ == "__main__":
     connection = instruqt.Instruqt(team="elastic-ilt")
     invites = connection.get_invites()
-    print(invites)
     test = [[invite.get("publicTitle"), invite.get("expiresAt"), invite.get("status"), len(invite.get("contentEdges"))] for invite in invites]
     print(tabulate(test, headers=["Public Title", "Expires At", "Status", "Number of tracks"], showindex=True))
 
